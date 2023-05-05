c23442cb07edb006b2d7178cba821f359d03b2c5
aamuley <anusha.muley@berkeley.edu>
2023-05-05 13:42:34 -0700

    q4 run kinda broke oops

--- models.py
+++ models.py
@@ -225,6 +225,14 @@ class LanguageIDModel(object):
 
         # Initialize your model parameters here
         "*** YOUR CODE HERE ***"
+        self.w1 = nn.Parameter(47, 100)
+        self.w2 = nn.Parameter(100, 20)
+        self.w3 = nn.Parameter(20, 5)
+        self.b1 = nn.Parameter(1, 100)
+        self.b2 = nn.Parameter(1, 20)
+        self.b3 = nn.Parameter(1, 5)
+
+
 
     def run(self, xs):
         """
@@ -256,6 +264,16 @@ class LanguageIDModel(object):
                 (also called logits)
         """
         "*** YOUR CODE HERE ***"
+        for i in xs:
+            z1_reg = nn.Linear(i ,self.w1)
+            z1 = nn.AddBias(z1_reg,self.b1)
+            a1 = nn.ReLU(z1)
+            z2_reg = nn.Linear(a1,self.w2)
+            z2 = nn.AddBias(z2_reg,self.b2)
+            a2 = nn.ReLU(z2)
+            z3_reg = nn.Linear(a2,self.w3)
+            z3 = nn.AddBias(z3_reg,self.b3)
+        return z3
 
     def get_loss(self, xs, y):
         """
@@ -272,9 +290,23 @@ class LanguageIDModel(object):
         Returns: a loss node
         """
         "*** YOUR CODE HERE ***"
+        x_batch = self.run(xs)
+        return nn.SoftmaxLoss(x_batch, y)
 
     def train(self, dataset):
         """
         Trains the model.
         """
         "*** YOUR CODE HERE ***"
+        val_acc = dataset.get_validation_accuracy()
+        while(val_acc < 0.98):
+            for x, y in dataset.iterate_once(200):
+                calc_loss = self.get_loss(x, y)
+                gw1, gw2, gw3, gb1, gb2, gb3 = nn.gradients(calc_loss, [self.w1, self.w2, self.b1, self.b2])
+                self.w1.update(gw1, -0.5)
+                self.w2.update(gw2, -0.5)
+                self.w3.update(gw3, -0.2)
+                self.b1.update(gb1, -0.5)
+                self.b2.update(gb2, -0.5)
+                self.b3.update(gb3, -0.2)
+                val_acc = dataset.get_validation_accuracy()
